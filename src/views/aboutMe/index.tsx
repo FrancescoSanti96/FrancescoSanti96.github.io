4ccce23ec88c62bb6048baa563ababe5774c488a
FrancescoSanti96 <francescosanti123@gmail.com>
2022-06-25 13:31:38 +0200

    slider about me

--- src/views/aboutMe/index.tsx
+++ src/views/aboutMe/index.tsx
@@ -1,46 +1,45 @@
-import image from "../../img/me.jpg"
+import imageMaggie from "../../img/maggie.jpg";
+import imageMaggieOcchiali from "../../img/maggieOcchiali.jpg";
+import meAndMaggie from "../../img/meAndMaggie.jpg";
 
 export default function AboutMe() {
-  return (
-    <>
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={image} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src={image} className="d-block w-100" alt="..." />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src={image} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-    </>
-  );
+    return (
+        <>
+            <div className="container">
+                <div id="carouselExampleInterval" className="carousel slide carousel-fade mt-2 border border-3" data-bs-ride="carousel" style={{ width: 'fit-content', maxHeight: 400, margin: 'auto' }}>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <img src={imageMaggie}
+                                className="d-block"
+                                alt="..."
+                                style={{ maxHeight: 400 }}
+                                />
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={imageMaggieOcchiali}
+                                className="d-block"
+                                alt="..."
+                                style={{ maxHeight: 400 }}
+                                />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={meAndMaggie }
+                                className="d-block"
+                                alt="..."
+                                style={{ maxHeight: 400 }}
+                                />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
