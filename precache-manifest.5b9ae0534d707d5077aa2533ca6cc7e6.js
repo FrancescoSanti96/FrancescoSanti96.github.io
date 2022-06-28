7cd165bfb7a89112cd9fdc15c52bf52353f61b0d
Francesco <francescosanti123@gmail.com>
2022-06-28 22:51:26 +0200

    Updates

---
+++ precache-manifest.5b9ae0534d707d5077aa2533ca6cc7e6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5521f0a90f839b44f6b",
+    "url": "/FrancescoSanti96.github.io/static/css/main.fd7f8668.chunk.css"
+  },
+  {
+    "revision": "f5521f0a90f839b44f6b",
+    "url": "/FrancescoSanti96.github.io/static/js/main.5c50486a.chunk.js"
+  },
+  {
+    "revision": "82ac9a783866e8e67112",
+    "url": "/FrancescoSanti96.github.io/static/js/runtime~main.00a36d47.js"
+  },
+  {
+    "revision": "17134d61e0ea2d6a555b",
+    "url": "/FrancescoSanti96.github.io/static/css/2.a81367a0.chunk.css"
+  },
+  {
+    "revision": "17134d61e0ea2d6a555b",
+    "url": "/FrancescoSanti96.github.io/static/js/2.b601d37a.chunk.js"
+  },
+  {
+    "revision": "5d06bb858cc186fdb179",
+    "url": "/FrancescoSanti96.github.io/static/js/3.b019360d.chunk.js"
+  },
+  {
+    "revision": "41778caac341766b1160ea66d3d13cbd",
+    "url": "/FrancescoSanti96.github.io/static/media/test.41778caa.pdf"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/FrancescoSanti96.github.io/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/FrancescoSanti96.github.io/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "2dd0170325043be77c72e09c1e943816",
+    "url": "/FrancescoSanti96.github.io/static/media/me.3684cb18.jpg"
+  },
+  {
+    "revision": "46bf3cf61d9dbc8e33d120c51a2a4073",
+    "url": "/FrancescoSanti96.github.io/static/media/alimenti.d029ab92.gif"
+  },
+  {
+    "revision": "e90b1b1bbb766685d4eb6415a35e2ea7",
+    "url": "/FrancescoSanti96.github.io/static/media/money.09443c15.gif"
+  },
+  {
+    "revision": "04504b2097f8546af003532131f0330c",
+    "url": "/FrancescoSanti96.github.io/static/media/maggie.0bd92212.jpg"
+  },
+  {
+    "revision": "a305a84103d67f2d874dd85be9ba0d7b",
+    "url": "/FrancescoSanti96.github.io/static/media/maggieOcchiali.efd1575e.jpg"
+  },
+  {
+    "revision": "a29b97df15c564671d861041a0c76aaf",
+    "url": "/FrancescoSanti96.github.io/static/media/meAndMaggie.2ef82fa6.jpg"
+  },
+  {
+    "revision": "356a9cfb6596a7d8f522d159c6f6be94",
+    "url": "/FrancescoSanti96.github.io/index.html"
+  }
+];
